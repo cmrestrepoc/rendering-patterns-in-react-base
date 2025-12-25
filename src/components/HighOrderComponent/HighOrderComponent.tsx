c4dbe63d3f33bc56d97812720d2f76934fcfec40
Carlos Restrepo <carlosrestrepo.dataart@latam.com>
2025-12-25 08:54:10 -0500

    clase 4 high order components

--- src/components/HighOrderComponent/HighOrderComponent.tsx
+++ src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,25 @@
+function withLoading<T extends object> (Component: React.ComponentType<T>) {
+  return (props: T & {isLoading: boolean}) => {
+    const { isLoading, ...rest } = props
+    return isLoading ? <p>Loading...</p> : <Component {...rest as T}/>
+  }
+}
+
+type UserProps = {
+  name: string;
+}
+
+const UserComponent: React.FC<UserProps> = ({name}) => {
+  return <p>Hell, {name}</p>
+} 
+
+const UserWithLoading = withLoading(UserComponent)
+
 export const ParentComponent = () => {
   return (
     <div>
+      <UserWithLoading isLoading={true} name="Carlos"/>
+      <UserWithLoading isLoading={false} name="Mario"/>
     </div>
   );
 };

3ce54c678cdf0fa4000707da7d29ef6b43884dd1
Carlos Restrepo <carlosrestrepo.dataart@latam.com>
2025-12-25 05:33:01 -0500

    tercera clase compound components

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,12 @@
+import Tab from "./Tab";
+import Tabs from "./Tabs";
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
+    <Tabs>
+      <Tab label="Jirafa">Jirafa</Tab>
+      <Tab label="Delfín">Delfín</Tab>
+      <Tab label="Lagarto">Lagarto</Tab>
+    </Tabs>
   );
 };
